4ead92c42b7aaa04a3341ebe70ef349f3bf2c7cd
Simon <krenich.szymon@gmail.com>
2023-05-20 14:20:24 +0200

    added readme, button styling correct

---
+++ README.md
@@ -0,0 +1,36 @@
+![Notes App GIF](notes_app.gif)
+
+## Features
+
+- Create a new note.
+- View a list of all notes.
+- View the details of an individual note.
+- User-friendly and intuitive interface.
+- Built with modern React.js with modular components.
+
+## Requirements
+
+- Node.js 12+
+- npm/yarn
+- React.js
+- react-router-dom
+- A running instance of the backend server
+
+## Easy Installation & Setup
+
+1. Clone the repository: 
+    ```
+    git clone https://github.com/username/repository.git
+    ```
+2. Navigate to the cloned repository: 
+    ```
+    cd repository
+    ```
+3. Install the necessary packages: 
+    ```
+    npm install
+    ```
+4. Run the application: 
+    ```
+    npm start
+    ```
--- src/components/routes/NoteOverlay.jsx
+++ src/components/routes/NoteOverlay.jsx
@@ -1,25 +1,26 @@
 import classes from './NoteOverlay.module.css';
 import Modal from '../Modal';
-import { Link, Form, redirect } from 'react-router-dom';
+import { Link, Form, redirect} from 'react-router-dom';
 import { useLoaderData } from 'react-router-dom';
 
 function NoteOverlay() {
-    const notes = useLoaderData();
+  const note = useLoaderData();
 
   return (
     <Modal>
-        <div className={classes.noteInfo}>
-            <h3>{notes.name}</h3>
-            <p>{notes.content}</p>
-        </div>
+      <div className={classes.noteInfo}>
+        <h3>{note.name}</h3>
+        <p>{note.content}</p>
+      </div>
     </Modal>
   );
 }
 
 export default NoteOverlay;
 
-export async function loader({params}){
-    const response = await fetch(`http://localhost:8080/posts/${params.id}`);
-    const data = await response.json();
-    return data.post;
+export async function loader({ params }) {
+  const response = await fetch(`http://localhost:8080/posts/${params.id}`);
+  const data = await response.json();
+  return data.post;
 }
+
